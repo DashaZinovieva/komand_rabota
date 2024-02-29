230d142d3c3d8d0469bbc6cfa86b7a012ceb3687
dashazinovieva94@gmail.com <zinovevad922@gmail.com>
2024-02-29 18:49:05 +0300

    Calculator

---
+++ 12/КАЛЬКУЛЯТОР.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12
+{
+    public partial class КАЛЬКУЛЯТОР : Form
+    {
+        public КАЛЬКУЛЯТОР()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
